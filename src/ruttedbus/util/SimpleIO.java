05f3038324eb70e3897a83710257516358089fb3
Josue Lopez <josue_lopez@prodigy.net>
2015-03-07 22:24:16 -0600

    Added double reading, and integer reading without prompt

--- src/ruttedbus/util/SimpleIO.java
+++ src/ruttedbus/util/SimpleIO.java
@@ -11,6 +11,28 @@ public final class SimpleIO {
     }
 
     /**
+     * @return Integer entered in console
+     */
+    public static int readInt() {
+        int input;
+        Scanner scanner;
+        while(true) {
+            try {
+                scanner = new Scanner(System.in);
+                //Getting input, exception occurs here
+                input = scanner.nextInt();
+                break;
+            }
+            catch(InputMismatchException e) {
+                // Use out stream instead of error stream, since error stream does not allow for consistent printing
+                System.out.println("Illegal integer format.");
+            }
+        }
+        scanner.close();
+        return input;
+    }
+
+    /**
      * Prompts user to enter an integer, until a valid integer is entered. Uses default radix.
      * @param prompt Prompt printed to console on each attempt to enter integer
      * @return Integer entered in console
@@ -29,6 +51,52 @@ public final class SimpleIO {
             catch(InputMismatchException e) {
                 // Use out stream instead of error stream, since error stream does not allow for consistent printing
                 System.out.println("Illegal integer format.");
+            }
+        }
+        scanner.close();
+        return input;
+    }
+
+    /**
+     * @return Double entered in console
+     */
+    public static double readDouble() {
+        double input;
+        Scanner scanner;
+        while(true) {
+            try {
+                scanner = new Scanner(System.in);
+                //Getting input, exception occurs here
+                input = scanner.nextDouble();
+                break;
+            }
+            catch(InputMismatchException e) {
+                // Use out stream instead of error stream, since error stream does not allow for consistent printing
+                System.out.println("Illegal double format.");
+            }
+        }
+        scanner.close();
+        return input;
+    }
+
+    /**
+     * @param prompt Prompts user to enter a double, until a valid double is entered.
+     * @return Double entered in console
+     */
+    public static double readDouble(String prompt) {
+        double input;
+        Scanner scanner;
+        while(true) {
+            try {
+                scanner = new Scanner(System.in);
+                System.out.print(prompt);
+                //Getting input, exception occurs here
+                input = scanner.nextDouble();
+                break;
+            }
+            catch(InputMismatchException e) {
+                // Use out stream instead of error stream, since error stream does not allow for consistent printing
+                System.out.println("Illegal double format.");
             }
         }
         scanner.close();
